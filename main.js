d27f302b8e13f14fbfd1febcf79d0944a4f7d3da
iqbolkhuja2009 <142048200+iqbolkhuja2009@users.noreply.github.com>
2023-12-15 19:04:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+// UYGA VAZIFA 1 - TAYYOR
+// let array = [
+//   {
+//     name: "john",
+//     age: 34,
+//   },
+//   {
+//     name: "adam",
+//     age: 15,
+//   },
+//   {
+//     name: "doe",
+//     age: 21,
+//   },
+//   {
+//     name: "john",
+//     age: 76,
+//   },
+// ];
+// let sum = 1;
+// for (let i = 0; i < array.length; i++) {
+//   sum = sum * array[i].age;
+// }
+// console.log(sum);
+// UYGA VAZIFA 2 - TAYYOR
+// let arr = [
+//   {
+//     name: "john",
+//     age: 34,
+//   },
+//   {
+//     name: "adam",
+//     age: 15,
+//   },
+//   {
+//     name: "doe",
+//     age: 21,
+//   },
+//   {
+//     name: "john",
+//     age: 76,
+//   },
+// ];
+// let sum = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i].age > 25) {
+//     console.log(arr[i]);
+//   }
+// }
+// UYGA VAZIFA 3 - TAYYOR
+// let array = [
+//   {
+//     name: "john",
+//     age: 34,
+//   },
+//   {
+//     name: "adam",
+//     age: 15,
+//   },
+//   {
+//     name: "doe",
+//     age: 21,
+//   },
+//   {
+//     name: "john",
+//     age: 76,
+//   },
+// ];
+// function age(arr) {
+//   let totalAge = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     totalAge += arr[i].age;
+//   }
+//   return totalAge / arr.length;
+// }
+
+// let allage = age(array);
+// console.log(O'rtacha yosh: ${allage});
+// UYGA VAZIFA 4 - TAYYOR
+// ASSALOMU ALAYKUM USTOZ MAN 4 - MASALANI FUNKSIYANI ICHIDA YOZA OLMADIM
+// let array = [
+//   {
+//     name: "john",
+//     age: 34,
+//   },
+//   {
+//     name: "adam",
+//     age: 15,
+//   },
+//   {
+//     name: "doe",
+//     age: 21,
+//   },
+//   {
+//     name: "john",
+//     age: 76,
+//   },
+// ];
+// let res;
+// if (array[0].age > array[1].age) {
+//   res = array[0].age;
+// } else {
+//   res = array[1].age;
+// }
+// if (array[2].age > res) {
+//   res = array[2].age;
+// }
+// if (array[3].age > res) {
+//   res = array[3].age;
+// }
+// console.log(res);
+// UYGA VAZIFA 5 - TAYYOR
+// let array = [
+//   {
+//     name: "john",
+//     age: 34,
+//   },
+//   {
+//     name: "adam",
+//     age: 15,
+//   },
+//   {
+//     name: "doe",
+//     age: 21,
+//   },
+//   {
+//     name: "john",
+//     age: 76,
+//   },
+// ];
+// let res;
+// if (array[0].age < array[1].age) {
+//   res = array[0].age;
+// } else {
+//   res = array[1].age;
+// }
+// if (array[2].age < res) {
+//   res = array[2].age;
+// }
+// if (array[3].age < res) {
+//   res = array[3].age;
+// }
+// console.log(res);
